4c120c6dcf63dfec9c61cd963cb6baee28c4b289
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-03 02:18:25 +0100

    Add shapes, calendar, stop_times and trips

--- osmtogtfs.py
+++ osmtogtfs.py
@@ -38,7 +38,8 @@ def cli(osmfile, outdir, zipfile):
     writer.add_agencies(processor.agencies.values())
     writer.add_stops(processor.stops.values())
     writer.add_routes(supported_routes)
-    populate_dummy_data(writer, supported_routes)
+    writer.add_shapes(processor.shapes)
+    populate_dummy_data(processor, writer, supported_routes)
 
     if zipfile:
         writer.write_zipped(os.path.join(outdir, zipfile))
@@ -49,38 +50,46 @@ def cli(osmfile, outdir, zipfile):
 
 
 def get_supported_routes(processor):
-    return [r for r in processor.all_routes if r['route_type'] in\
-    [GTFSRouteType.Bus.value,
-     GTFSRouteType.Tram.value,
-     GTFSRouteType.Subway.value,
-     GTFSRouteType.Rail.value]]
+    route_types = [GTFSRouteType.Bus.value,
+                   GTFSRouteType.Tram.value,
+                   GTFSRouteType.Subway.value,
+                   GTFSRouteType.Rail.value]
+    return [route for bulk in processor.routes.values() for version, route in bulk.values()
+            if route['route_type'] in route_types]
 
 
-def populate_dummy_data(writer, supported_routes):
-    trips = create_dummy_trips(supported_routes)
+def populate_dummy_data(processor, writer, routes):
+    calendar = create_dummy_calendar()
+    trips = create_dummy_trips(routes, calendar)
     writer.add_trips(trips)
     writer.add_stop_times(create_dummy_stoptimes(trips))
-    writer.add_calendar(create_dummy_calendar())
+    writer.add_calendar(calendar)
 
 
-def create_dummy_trips(routes):
-  return [{'route_id': '', 'service_id':'', 'trip_id':''}]
-
+def create_dummy_trips(routes, calendar):
+    for route in routes:
+        for cal in calendar:
+            yield {'route_id': route['route_id'],
+                   'service_id': cal['service_id'],
+                   'trip_id': 'trp_{}'.format(route['route_id']),
+                   'shape_id': 'shp_{}'.format(route['route_id'])}
 
 
 def create_dummy_stoptimes(trips):
-  return [{'trip_id': '', 'arrival_time': '', 'departure_time': '', 'stop_id': '',
-           'stop_sequence':''}]
+    for trip in trips:
+        yield {'trip_id': trip['trip_id'],
+               'arrival_time': '0:06:10',
+               'departure_time': '0:06:10',
+               'stop_id': '',
+               'stop_sequence': ''}
 
 
 def create_dummy_calendar():
-  return [{'service_id': '', 'monday': '', 'tuesday': '', 'wednesday': '', 'thursday': '',
-           'friday': '', 'saturday': '', 'sunday': '', 'start_date': '', 'end_date': ''}]
+    return [{'service_id': 'WE', 'monday': 0, 'tuesday': 0, 'wednesday': 0, 'thursday': 0,
+             'friday': 0, 'saturday': 1, 'sunday': 1, 'start_date': 20170101, 'end_date': 20190101},
+            {'service_id': 'WD', 'monday': 1, 'tuesday': 1, 'wednesday': 1, 'thursday': 1,
+             'friday': 1, 'saturday': 0, 'sunday': 0, 'start_date': 20170101, 'end_date': 20190101}]
 
 
 if __name__ == '__main__':
     cli()
-
-
-                
-

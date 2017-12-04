f664ea3c2c4f3e538903f7c683b9666aad5225e6
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-04 15:52:21 +0100

    Generate more dummy trips per service day.
    Former-commit-id: f249ce8eec38f98840154159479e989379400f39

--- gtfs_dummy.py
+++ gtfs_dummy.py
@@ -1,12 +1,13 @@
 """Tools to generate dummy GTFS feeds."""
 import datetime
+from functools import lru_cache
 
 
 def populate_dummy_data(writer, processor):
     calendar = _create_dummy_calendar()
-    trips = _create_dummy_trips(processor, calendar)
+    trips, stoptimes = _create_dummy_trips_and_stoptimes(processor, calendar)
     writer.add_trips(trips)
-    writer.add_stop_times(_create_dummy_stoptimes(trips, processor.stops))
+    writer.add_stop_times(stoptimes)
     writer.add_calendar(calendar)
 
 
@@ -17,41 +18,49 @@ def _create_dummy_calendar():
              'friday': 1, 'saturday': 0, 'sunday': 0, 'start_date': 20170101, 'end_date': 20190101}]
 
 
-def _create_dummy_trips(processor, calendar):
+def _create_dummy_trips_and_stoptimes(processor, calendar):
     trips = []
+    stoptimes = []
     for route_id, route in processor.routes.items():
         for cal in calendar:
-        # TODO: Create multiple dummy trips per route, e.g. every 20 minutes
-            trips.append(
-                {'route_id': route_id,
-                 'service_id': cal['service_id'],
-                 'trip_id': 'trp_{}'.format(route_id),
-                 'trip_headsign': '[Dummy]{}'.format(route['route_long_name']),
-                 'shape_id': route_id}
-                )
-    return trips
+        # For the sake of simplicity, we assume a fixed number of trips per service day.
+        # Even though in relaity there are less number of trips on weekends and holidays.
+        # We assume trips begin from 5:00 AM and run untill 11:00 PM and there is one trip
+        # every 20 minutes. Therefore in total we add 54 trips per route per service day.
+        # 18 service hours per day * 3 trips per hour = 54
+            for idx in range(54):
+                trip_id = '{route_id}{sequence}'.format(route_id=route_id, sequence=idx+1)
+                trip = {'route_id': route_id,
+                        'service_id': cal['service_id'],
+                        'trip_id': trip_id,
+                        'trip_headsign': '[Dummy]{}'.format(route['route_long_name']),
+                        'shape_id': route_id}
+                trips.append(trip)
+                stoptimes.extend(_create_dummy_trip_stoptimes(trip, idx, processor))
+    return trips, stoptimes
 
 
-def _create_dummy_stoptimes(trips, stops):
+def _create_dummy_trip_stoptimes(trip, sequence, processor):
     """Create station stop times for each trip."""
     # stops = find stops that belong to this route (trip)
     # needs trip_id and stop_id
-    first_service_time = datetime.datetime(2017, 1, 1, 5, 0, 0)
-    for trip in trips:
-        stop_sequence = 0
-        arrival = first_service_time
-        for stop in _get_route_stops(trip['route_id'], stops):
-            departure = arrival + datetime.timedelta(seconds=30)
-            yield {'trip_id': trip['trip_id'],
-                   'arrival_time': arrival.strftime('%H:%M:%S'),
-                   'departure_time': departure.strftime('%H:%M:%S'),
-                   'stop_id': stop['stop_id'],
-                   'stop_sequence': stop_sequence}
-            stop_sequence += 1
-            arrival += datetime.timedelta(minutes=20)
-        first_service_time += datetime.timedelta(minutes=20)
+    delta = sequence*datetime.timedelta(minutes=20)
+    first_service_time = datetime.datetime(2017, 1, 1, 5, 0, 0) + delta
+
+    stop_sequence = 0
+    arrival = first_service_time
+    for stop in _get_route_stops(trip['route_id'], processor):
+        departure = arrival + datetime.timedelta(seconds=30)
+        yield {'trip_id': trip['trip_id'],
+               'arrival_time': arrival.strftime('%H:%M:%S'),
+               'departure_time': departure.strftime('%H:%M:%S'),
+               'stop_id': stop['stop_id'],
+               'stop_sequence': stop_sequence}
+        stop_sequence += 1
+        arrival += delta
 
 
-def _get_route_stops(route_id, stops):
-    #return filter(lambda x: x['route_id'] == route_id, stops.values())
-    return [stop for stop in stops.values() if stop['route_id'] == route_id]
+@lru_cache()
+def _get_route_stops(route_id, processor):
+    for stop_id in processor.route_stops[route_id]:
+        yield processor.stops[stop_id]

8f9de2a43a2319106629cd2fe8547064b31e5981
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-06 23:51:59 +0100

    Don't expand zipfile path, fix log msg.
    Former-commit-id: 92d5fc13104985d2bf9d2379c331a0a0918dee56

--- osmtogtfs.py
+++ osmtogtfs.py
@@ -15,10 +15,7 @@ from _osmtogtfs.gtfs_dummy import populate_dummy_data
 @click.command()
 @click.argument('osmfile', type=click.Path(exists=True, readable=True))
 @click.option('--outdir', default='.',
-              type=click.Path(exists=True,
-                              dir_okay=True,
-                              writable=True,
-                              resolve_path=True),
+              type=click.Path(),
               help='Output directory. Default is the current directory.')
 @click.option('--zipfile',
               type=click.Path(file_okay=True,
@@ -51,7 +48,7 @@ def cli(osmfile, outdir, zipfile, dummy, loglevel):
 
     if zipfile:
         writer.write_zipped(os.path.join(outdir, zipfile))
-        click.echo('GTFS feed saved in %s' % zipfile)
+        click.echo('GTFS feed saved in %s' % os.path.join(outdir, zipfile))
     else:
         writer.write_unzipped(outdir)
         click.echo('GTFS feed saved in %s' % outdir)

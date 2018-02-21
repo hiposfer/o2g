4b59cc1790ca5a88b8390a28d04378a9aa1c834c
Mehdi Sadeghi <msk1361@gmail.com>
2018-02-21 12:55:56 +0100

    Patch agency urls and tzones in dummy mode.
    Former-commit-id: 3bf5559b7712b32ff01b7f21bf0a7385ae1832c8 [formerly 7c4cbc35f17f499c11af152e566dc3311b6d12bd]
    Former-commit-id: 92ef190a339a6834c9d2f3cf1932f0a549a0284c [formerly 2c015223ee5b4c32de2c47c7a8addd24ce0733c4]
    Former-commit-id: 3f5a7f4af6369204c530daaae455a2cff3d9c504

--- osmtogtfs.py
+++ osmtogtfs.py
@@ -44,16 +44,17 @@ def cli(osmfile, outdir, zipfile, dummy, loglevel):
     logging.debug('Preprocessing took %d seconds.', (time.time() - start))
 
     writer = GTFSWriter()
-    writer.add_agencies(processor.agencies.values())
-    writer.add_stops(processor.stops.values())
-    writer.add_routes(processor.routes.values())
-    writer.add_shapes(processor.shapes)
 
     if dummy:
         _populate_dummy_data(writer,
             processor.routes,
             processor.stops,
             processor.route_stops)
+        _patch_agencies(processor.agencies)
+
+    writer.add_agencies(processor.agencies.values())
+    writer.add_stops(processor.stops.values())
+    writer.add_routes(processor.routes.values())
 
     if zipfile:
         writer.write_zipped(os.path.join(outdir, zipfile))
@@ -71,6 +72,14 @@ def _populate_dummy_data(writer, routes, stops, route_stops):
     writer.add_stop_times(dummy.stop_times)
     writer.add_calendar(dummy.calendar)
     writer.add_shapes(dummy.shapes)
+
+
+def _patch_agencies(agencies):
+    for agency in agencies.values():
+        if 'agency_url' not in agency or not agency['agency_url']:
+            agency['agency_url'] = 'http://hiposfer.com'
+        if 'agency_timezone' not in agency or not agency['agency_timezone']:
+            agency['agency_timezone'] = 'Europe/Berlin'
 
 
 if __name__ == '__main__':

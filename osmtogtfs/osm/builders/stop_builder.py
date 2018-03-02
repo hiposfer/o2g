20123e42b54604836c4747fad27cac5118b3d6c8
Mehdi Sadeghi <msk1361@gmail.com>
2018-03-02 20:23:28 +0100

    Get rid of dup stop_ids
    Former-commit-id: 527e848e7ea4b8514cfcb51ca9e17f1d406afc2a

--- osmtogtfs/osm/builders/stop_builder.py
+++ osmtogtfs/osm/builders/stop_builder.py
@@ -3,16 +3,19 @@ from osmtogtfs.osm.models import Stop
 
 
 def build_stops(relations, nodes):
+    visited_stops_ids = set()
     for rel in relations.values():
-        for stop in extract_stops(rel, nodes):
+        for stop in extract_stops(rel, nodes, visited_stops_ids):
             if stop:
                 yield stop
 
 
-def extract_stops(relation, nodes):
+def extract_stops(relation, nodes, visited_stops_ids):
     """Extract stops in a relation."""
     for member_id, member_role in relation.member_info:
-        if _is_stop(member_id, member_role, nodes):
+        if member_id not in visited_stops_ids and \
+            _is_stop(member_id, member_role, nodes):
+            visited_stops_ids.add(member_id)
             if member_id in nodes:
                 yield Stop(member_id,
                     nodes[member_id].tags.get('name') or\

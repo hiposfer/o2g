6010329f692d70af8eccdaba7b389c4573d3b97a
Mehdi Sadeghi <msk1361@gmail.com>
2018-03-02 22:10:00 +0100

    Fix guessing agency timezone but disable it.
    Guessing timezone causes validation errors at the moment.
    See #28.
    
    
    Former-commit-id: 3e8bec41f3560acd59ef01a37d5a71dfef41c490 [formerly 2469cee982fe18216487a012cd6f7f14b66e8ee1]
    Former-commit-id: 96a5f7cef70d67373f5ca4f58e69e42759bddaac [formerly c6e7205b2ea4de7c86a7bf23f51ab3fca41419cb]
    Former-commit-id: 608a9273f2bb35b8b5cdb94a63e5a0a2720c46d7

--- osmtogtfs/osm/builders/agency_builder.py
+++ osmtogtfs/osm/builders/agency_builder.py
@@ -1,19 +1,25 @@
 """Functionality to build a list of agencies."""
+import logging
 import hashlib
+
+from timezonefinder import TimezoneFinder
 
 from osmtogtfs.osm.models import Agency
 
 
-def build_agencies(relations):
+TZ_FINDER = TimezoneFinder()
+
+
+def build_agencies(relations, nodes, ways):
     extracted = []
     for rel in relations.values():
-        agency = extract_agency(rel)
+        agency = build_agency(rel, nodes)
         if agency and agency.agency_id not in extracted:
             extracted.append(agency.agency_id)
-            yield agency
+            yield agency#._replace(agency_timezone=_guess_timezone(rel, nodes, ways))
 
 
-def extract_agency(relation):
+def build_agency(relation, nodes):
     """Extract agency information."""
     # TODO: find out the operator for routes without operator tag.
     # See: http://wiki.openstreetmap.org/wiki/Key:operator
@@ -30,27 +36,27 @@ def extract_agency(relation):
 
     agency_id = int(hashlib.sha256(op.encode('utf8')).hexdigest(), 16) % 10**8
 
-    return Agency(agency_id, '', op, '')#self._guess_timezone(relation)}
+    return Agency(agency_id, '', op, '')
 
 
-# def _guess_timezone(self, relation):
-#     """Guess timezone of the relation by looking at it's nodes."""
-#     lon, lat = self._get_first_coordinate(relation)
-#     timezone = self.tzfinder.timezone_at(lng=lon, lat=lat)
-#     if not timezone:
-#         logging.debug('No timezone found for (%s, %s)', lon, lat)
-#     return timezone
+def _guess_timezone(relation, nodes, ways):
+    """Guess timezone of the relation by looking at it's nodes."""
+    lon, lat = _get_first_coordinate(relation, nodes, ways)
+    timezone = TZ_FINDER.timezone_at(lng=lon, lat=lat)
+    if not timezone:
+        logging.debug('No timezone found for (%s, %s)', lon, lat)
+    return timezone
 
-# def _get_first_coordinate(self, relation):
-#     for member in relation.members:
-#         if member.ref in self.nodes:
-#             return self.nodes[member.ref].lon, self.nodes[member.ref].lat
-#         elif member.ref in self.ways:
-#             return self._get_first_way_coordinate(member.ref)
-#     logging.debug('No node found for relation %s', relation.id)
-#     return 0, 0
+def _get_first_coordinate(relation, nodes, ways):
+    for member_id, member_role in relation.member_info:
+        if member_id in nodes:
+            return nodes[member_id].lon, nodes[member_id].lat
+        elif member_id in ways:
+            return _get_first_way_coordinate(member_id, ways)
+    logging.debug('No node found for relation %s', relation.id)
+    return 0, 0
 
-# def _get_first_way_coordinate(self, way_id):
-#     if way_id in self.ways and self.ways[way_id]:
-#         # Pick the first node
-#         return self.ways[way_id][0][0], self.ways[way_id][0][1]
+def _get_first_way_coordinate(way_id, ways):
+    if way_id in ways:
+        # Pick the first node
+        return ways[way_id].points[0]

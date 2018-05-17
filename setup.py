5f3599dfc9cce16354edd83ca9bc8cead09c1475
Mehdi Sadeghi <msk1361@gmail.com>
2018-05-17 20:14:13 +0200

    Add readme to the distribution. Fix #78 v0.2.3
    Former-commit-id: 38cccac35c2d398c304e965335289b899f45aa0b

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='osmtogtfs',
-      version='0.3.1',
+      version='0.3.2',
       description='Extracts partial GTFS feed from OSM data.',
       long_description=readme(),
       author='Mehdi Sadeghi',

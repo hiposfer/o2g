c57e253cf9303f37ab8aa7a44a2ad6334d16af9e
Mehdi Sadeghi <msk1361@gmail.com>
2018-09-02 20:07:17 +0200

    Add error logs when member info are missing.
    Former-commit-id: e6697d891a9e3da428fa021c71b3c1dfb7ef2df4

--- osmtogtfs/osm/exporter.py
+++ osmtogtfs/osm/exporter.py
@@ -1,3 +1,5 @@
+import logging
+
 from osmtogtfs.osm.handlers import RelationHandler, NodeHandler, WayHandler
 from osmtogtfs.osm.builders import build_routes, build_stops, build_agencies,\
     build_shapes
@@ -33,16 +35,58 @@ class TransitDataExporter(object):
         self.rh = RelationHandler()
         self.rh.apply_file(self.filename)
 
+        logging.debug('Found %d public transport relations.', len(self.rh.relations))
+
         # Collect ids of interest
-        ids = {}
-        for rel in self.rh.relations.values():
-            for nid, _ in rel.member_info:
-                ids[nid] = None
+        node_ids, stop_node_ids, way_ids, reverse_map = self.__collect_ids()
 
         # Extract nodes
-        self.nh = NodeHandler(ids)
+        self.nh = NodeHandler(node_ids)
         self.nh.apply_file(self.filename, locations=True)
 
+        count = 0
+        for idx, missing_node_id in enumerate(self.nh.missing_node_ids):
+            count += 1
+            logging.warn(
+                '[no data] stop node: rel %s ref %s.',
+                reverse_map[missing_node_id], missing_node_id)
+
+        if count:
+            logging.warn(
+                '%d nodes that appear in relations are missing.',
+                count)
+        else:
+            logging.debug('Lucky you! All relation member nodes were found.')
+
         # Extract ways
-        self.wh = WayHandler(ids)
+        self.wh = WayHandler(way_ids)
         self.wh.apply_file(self.filename, locations=True)
+
+    def __collect_ids(self):
+        node_ids = set()
+        stop_node_ids = set()
+        way_ids = set()
+        reverse_map = {}
+
+        for rel in self.rh.relations.values():
+            for mtype, ref, role in rel.member_info:
+                reverse_map[ref] = rel.id
+
+                if mtype in ['n', 'node']:
+                    node_ids.add(ref)
+                    if role in ['stop', 'platform']:
+                        stop_node_ids.add(ref)
+
+                elif mtype in ['w', 'way']:
+                    way_ids.add(ref)
+
+                elif mtype in ['r', 'relation']:
+                    logging.warning(
+                        '[Rel: %s]: super-relations are not supported yet. ref: %s',
+                        rel.id, ref)
+                else:
+                    logging.warning(
+                        '[Rel: %s]: unknown member type %s, ref: %s',
+                        rel.id, mtype, ref)
+
+        return node_ids, stop_node_ids, way_ids, reverse_map

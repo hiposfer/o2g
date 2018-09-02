a445fe762041dc2e87ecb6d06f070c3a99f9f6f2
Mehdi Sadeghi <msk1361@gmail.com>
2018-09-02 20:07:17 +0200

    Add error logs when member info are missing.
    Former-commit-id: c57e253cf9303f37ab8aa7a44a2ad6334d16af9e [formerly e6697d891a9e3da428fa021c71b3c1dfb7ef2df4]
    Former-commit-id: 5de7c500988a7be9325b239eb54e4373ed877b58

--- osmtogtfs/osm/builders/shape_builder.py
+++ osmtogtfs/osm/builders/shape_builder.py
@@ -6,8 +6,7 @@ from osmtogtfs.osm.models import Shape
 
 def build_shapes(relations, nodes, ways):
     for rel in relations.values():
-        route_shape = build_shape(rel, nodes, ways)
-        for record in route_shape:
+        for record in build_shape(rel, nodes, ways):
             if record:
                 yield record
 
@@ -16,20 +15,20 @@ def build_shape(relation, nodes, ways):
     """Extract shape of one route."""
     sequence_index = 0
 
-    for member_id, _ in relation.member_info:
-
-        shape = None
+    for member_type, member_id, member_role in relation.member_info:
 
         if member_id in nodes:
-            shape = Shape(
+            yield Shape(
                 relation.id,
                 nodes[member_id].lat,
                 nodes[member_id].lon,
                 sequence_index)
+
             sequence_index += 1
 
         # Do we need to consider ways too? It dramatically increases the number of shapes.
-        # elif member_id in ways:
+        elif member_id in ways:
+            continue
         #     for point in ways[member_id].points:
         #         shape = Shape(
         #             relation.id,
@@ -39,7 +38,5 @@ def build_shape(relation, nodes, ways):
         #         sequence_index += 1
 
         else:
-            logging.warning('Relation [%s] member [%s] not available.',
-                            relation.id, member_id)
-
-        yield shape
+            logging.warn('[no data] rel %s type %s ref %s.',
+                          relation.id, member_type, member_id)

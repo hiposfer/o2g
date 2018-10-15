ba0ec36a9cbe31e56ca0aba35229ef8771f238e6
Mehdi Sadeghi <mehdi@mehdix.org>
2018-10-15 17:10:53 +0200

    Bump version 0.4.1.
    Former-commit-id: 2bf353952158c15815da266608a3749cba10a225 [formerly 4d8b3dd9bc2af04d3436802d0649941a10fbecb3]
    Former-commit-id: e33488736818b5014d014b13d9ca6731b506e147

--- osmtogtfs/__init__.py
+++ osmtogtfs/__init__.py
@@ -3,4 +3,4 @@ osmtogtfs is a tool to extract public transport information from OpenStreetMap
 data. osmtogtfs comes with a client tool called o2g.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"

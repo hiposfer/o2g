b08b41621054edfabc85c3bdfcf0f17e663d4a85
Mehdi Sadeghi <mehdi@mehdix.org>
2018-10-15 12:18:09 +0200

    Rewrite the web app with bottle.
    Former-commit-id: 4f6cb126bc30027e04dfb9d55ac275c46760644f

--- demo/app.py
+++ demo/app.py
@@ -2,14 +2,11 @@
 import os
 import pathlib
 import tempfile
-import urllib.parse
-import urllib.request
-
+import urllib
+from urllib.parse import urlparse
+from urllib.request import urlopen
 
 from bottle import run, template, request, static_file, abort, default_app
-
-import requests
-import validators
 
 from osmtogtfs.cli import main
 
@@ -25,21 +22,26 @@ def index():
 
 @app.post('/')
 def do_index():
-    uploaded_filepath = \
+    filepath = \
         save_file(request.files['file']) if 'file' in request.files else None
 
     url = request.forms.get('url')
 
-    if not url and not uploaded_filepath:
+    if not url and not filepath:
         return template('index.html', messages=['Provide a URL or upload a file.'])
 
-    if not uploaded_filepath and not validators.url(url):
+    if not filepath and not is_valid_url(url):
         return template('index.html', messages=['Not a valid URL.'])
 
-    filename = uploaded_filepath or dl_osm_from_url(url)
-    zipfile = create_zipfeed(filename, bool(request.forms.get('dummy')))
+    if not filepath:
+        dl_filename, dl_filepath = dl_osm_from_url(url)
 
-    return static_file(os.path.split(zipfile)[-1], root=UPLOAD_DIR, download=True)
+    filename = os.path.split(filepath)[-1] if filepath else dl_filename
+    return send_feed(filepath or dl_filepath, filename + '.gtfs.zip')
+
+
+def is_valid_url(url):
+    return urlparse(url).scheme in ('http', 'https')
 
 
 def save_file(file):
@@ -48,6 +50,18 @@ def save_file(file):
         filepath = os.path.join(UPLOAD_DIR, filename)
         file.save(filepath, overwrite=True)
         return filepath
+
+
+def send_feed(filepath, filename=None):
+    zipfile = create_zipfeed(filepath, bool(request.forms.get('dummy')))
+    if not filename:
+        filename = os.path.split(zipfile)[-1]
+
+    return static_file(
+        os.path.split(zipfile)[-1],
+        root=os.path.split(zipfile)[0],
+        download=filename,
+        mimetype='application/zip')
 
 
 def create_zipfeed(filename, dummy=False):
@@ -59,26 +73,20 @@ def create_zipfeed(filename, dummy=False):
 
 @app.get('/o2g', methods=['GET'])
 def o2g():
-    area = request.args.get('area')
-    bbox = request.args.get('bbox')
-    url = request.args.get('url')
+    area = request.params.get('area')
+    bbox = request.params.get('bbox')
+    url = request.params.get('url')
 
     if area or bbox:
-        filepath = dl_osm_from_overpass(area, bbox)
-    elif url and validators.url(url):
-        filepath = dl_osm_from_url(url)
+        filename, filepath = dl_osm_from_overpass(area, bbox)
+        if area:
+            filename = area + '_overpass'
+    elif url and is_valid_url(url):
+        filename, filepath = dl_osm_from_url(url)
     else:
         abort(400)
 
-    zipfile = create_zipfeed(
-        filepath,
-        bool(request.args.get('dummy')))
-
-    return static_file(
-        os.path.split(zipfile)[-1],
-        root=UPLOAD_DIR,
-        #mimetype='application/zip',
-        download=True)
+    return send_feed(filepath, filename + '.gtfs.zip')
 
 
 def dl_osm_from_overpass(area, bbox):
@@ -86,16 +94,19 @@ def dl_osm_from_overpass(area, bbox):
         raise Exception('At lease area or bbox must be given.')
 
     overpass_query = build_overpass_query(area, bbox)
-    overpass_api = "http://overpass-api.de/api/interpreter"
+    overpass_api_url = "http://overpass-api.de/api/interpreter"
 
-    filename = tempfile.mktemp(suffix='_overpass.osm')
-    resp = requests.post(overpass_api, data=overpass_query)
-    if resp.ok:
-        with open(filename, 'w') as osm:
-            osm.write(resp.content.decode('utf-8'))
-        return filename
+    filepath = tempfile.mktemp(suffix='_overpass.osm')
+    resp = urlopen(overpass_api_url, overpass_query.encode('utf-8'))
+
+    if resp.status == 200:
+        with open(filepath, 'w') as osm:
+            osm.write(resp.read().decode('utf-8'))
+        return os.path.split(filepath)[-1], filepath
     else:
-        resp.raise_for_status()
+        raise urllib.request.HTTPError(
+            'Error calling Overpass API. Status: {}, reason: {}'.format(
+                resp.status, resp.reason))
 
     raise Exception("Can't download data form overpass api.")
 
@@ -131,10 +142,10 @@ def build_overpass_query(area, bbox):
 
 def dl_osm_from_url(url):
     filename = tempfile.mktemp(suffix=pathlib.Path(url).name)
-    local_filename, headers =\
+    filepath, headers =\
         urllib.request.urlretrieve(url, filename=filename)
-    print(local_filename, headers)
-    return local_filename
+    print(filepath, headers)
+    return pathlib.Path(url).name, filepath
 
 
 if __name__ == '__main__':

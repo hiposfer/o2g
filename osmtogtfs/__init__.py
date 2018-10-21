a58be61398b87e35546ca239c8947e58779d7dd2
Mehdi Sadeghi <mehdi@mehdix.org>
2018-10-21 19:20:26 +0200

    Update tag line docstring.
    Former-commit-id: cd566fd5042261fd9586947cf6812cf413cde4d0 [formerly bafb00b9ed447c5aff9b505e235e28d64efaf0c6]
    Former-commit-id: 355dcbce05f874a2adb83e28c0a47de634e33964

--- osmtogtfs/__init__.py
+++ osmtogtfs/__init__.py
@@ -1,6 +1,5 @@
 """
-osmtogtfs is a tool to extract public transport information from OpenStreetMap
-data. osmtogtfs comes with a client tool called o2g.
+A simple tool to extract public transport data from OpenStreetMap.
 """
 
 __version__ = "0.4.3"

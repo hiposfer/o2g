66bea262e8afc48401ea6b92417236c7c27167bd
Mehdi Sadeghi <msk1361@gmail.com>
2018-11-30 22:51:30 +0100

    WIP: consider stop_area.

--- o2g/osm/builders/stop_builder.py
+++ o2g/osm/builders/stop_builder.py
@@ -14,14 +14,21 @@ def build_stops(relations, nodes):
 
 def extract_stops(relation, nodes, visited_stop_ids):
     """Extract stops in a relation."""
+    is_stop_area = relation.tags.get('public_transport') == 'stop_area'
+    # member_role: stop, platform, terminal, stop_position, etc.
+    # stop_area, station, stop_position
     for member_type, member_id, member_role in relation.member_info:
 
         if member_id not in visited_stop_ids and \
             member_id in nodes and\
-                _is_stop(member_id, member_role, nodes):
+                member_role in ('stop', 'platform'):
+                #  _is_stop(member_id, member_role, nodes)
+
+            location_type = ''
+            parent_station = ''
 
             visited_stop_ids.add(member_id)
-            yield Stop(
+            latest_stop = Stop(
                 member_id,
                 nodes[member_id].tags.get('name') or
                 "Unnamed {} stop.".format(relation.tags.get('route')),
@@ -29,8 +36,28 @@ def extract_stops(relation, nodes, visited_stop_ids):
                 nodes[member_id].lat if member_id in nodes else '',
                 relation.id,
                 _map_wheelchair(nodes[member_id].tags.get('wheelchair')),
-                '',  # location_type
-                '')  # parent_station
+                location_type,
+                parent_station)
+            yield latest_stop
+
+    # One stop per stop_area is necessary.
+    if is_stop_area:
+        # For the time being we use the position of the latest
+        # stop in the stop_area.
+        yield _stop_area(relation, latest_stop)
+
+
+def _stop_area(relation, latest_stop):
+    return Stop(
+        relation.id,
+        relation.tags.get('name') or
+        "Unnamed stop_area.",
+        latest_stop.stop_lon,
+        latest_stop.stop_lat,
+        relation.id,
+        0,
+        1,  # A station in GTFS terms
+        '')  # Blank values since stations can't contain other stations.
 
 
 def _is_stop(member_id, member_role, nodes):
